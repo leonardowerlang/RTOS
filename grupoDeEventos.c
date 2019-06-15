52baf1aa5a027df4db3e984220bdbc3932713eeb
Leonardo Werlang <werlangleonardo@gmail.com>
2019-06-15 17:46:06 -0300

    :hammer: Adiciona concorrência de processos ao grupo de eventos

--- grupoDeEventos.c
+++ grupoDeEventos.c
@@ -1,55 +1,63 @@
 #include <Arduino_FreeRTOS.h>
 #include <event_groups.h>
+#include <queue.h>
 
+QueueHandle_t cadeiras = NULL;
 EventGroupHandle_t clientes;
 
+void vPrintString(const char *pcString){
+	vTaskSuspendAll();
+	{
+      Serial.println(pcString);
+      Serial.flush();
+	}
+	xTaskResumeAll();
+	if(Serial.available()){
+		vTaskEndScheduler();
+	}
+}
+
+
 void cortarCabelo(){
-  Serial.println("Cortando Cabelo");
+  vPrintString("Cortando Cabelo");
   vTaskDelay(5000 / portTICK_PERIOD_MS);
-  Serial.println("Acabou de cortar o cabelo");
+  vPrintString("Acabou de cortar o cabelo");
 }
 
 static void barbeiro(void* params){
+  int id;
   while(true){
-		int temp = xEventGroupWaitBits(clientes, B111, pdTRUE, pdFALSE, portMAX_DELAY);
-		if(temp == B111){
-		  xEventGroupSetBits(clientes, B011);
-		}else if(temp == B011){
-			xEventGroupSetBits(clientes, B001);
-		}else if(temp == B001){
-			xEventGroupSetBits(clientes, B000);
-		}
-    cortarCabelo();
+    if(xQueueReceive(cadeiras, &id, portMAX_DELAY) == pdPASS){
+      cortarCabelo();
+      xEventGroupSetBits(clientes, id);
+    }
   }
 }
 
 static void cliente(void* params){
+  int id = (int)params;
   while(true){
-		Serial.println("Cliente -> Chegou");
-		int temp = xEventGroupWaitBits(clientes, B111, pdFALSE, pdFALSE, 0);
-		if(temp <= B000){
-		  xEventGroupSetBits(clientes, B001);
-			Serial.println("Cliente -> Primeiro da fila");
-		}else if(temp == B001){
-			xEventGroupSetBits(clientes, B011);
-			Serial.println("Cliente -> Segundo da fila");
-		}else if(temp == B011){
-			xEventGroupSetBits(clientes, B111);
-			Serial.println("Cliente -> Ultimo da fila");
-		}else{
-      Serial.println("Cliente -> Falou troxa");
+    vTaskDelay(random(2000, 5000) / portTICK_PERIOD_MS);
+    if(xQueueSendToBack(cadeiras, &id, 0) == pdPASS){
+      vPrintString("Cliente-> Fila!");
+      xEventGroupWaitBits(clientes, id, pdTRUE, pdTRUE, portMAX_DELAY);
+    }else{
+      vPrintString("Cliente-> Foi embora!");
     }
-    vTaskDelay(random(10000) / portTICK_PERIOD_MS);
   }
 }
 
 void setup(){
   Serial.begin(9600);
 
-	clientes = xEventGroupCreate();
+  clientes = xEventGroupCreate();
+  cadeiras = xQueueCreate(2, sizeof(int));
 
-  xTaskCreate(cliente, "Cliente", 100, NULL, 1, NULL);
   xTaskCreate(barbeiro, "Barbeiro", 100, NULL, 1, NULL);
+  xTaskCreate(cliente, "Cliente", 100, (void*)1, 1, NULL);
+  xTaskCreate(cliente, "Cliente", 100, (void*)2, 1, NULL);
+  xTaskCreate(cliente, "Cliente", 100, (void*)4, 1, NULL);
+  xTaskCreate(cliente, "Cliente", 100, (void*)8, 1, NULL);
   vTaskStartScheduler();
   for(;;);
 }
